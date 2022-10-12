7105a7ddc62307a4fe68ea30e4beaddc2e42fa12
Artem Ryzhov <artem-from-spb@yandex.ru>
2022-10-12 11:07:48 +0300

    logger added

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const routes = require('./routes/index');
 const defaultErrorHandler = require('./middlewares/defaultErrorHandler');
@@ -22,7 +23,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
 app.use(routes);
+
+app.use(errorLogger);
 
 app.use(errors());
 
