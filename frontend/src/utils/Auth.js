796b8bdd86b426dd6d5dfb087f44aba90d6b9ca6
Artem Ryzhov <artem-from-spb@yandex.ru>
2022-10-17 19:37:25 +0300

    no reload fix25

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -35,7 +35,7 @@ export function checkData(token) {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${token}`,
     },
   }).then(checkResponse);
 }
